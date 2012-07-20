2409eaa23c6d4e57625f9f0527b327a73fa27bce
mfilmer <mattfilmer@gmail.com>
2012-07-20 11:31:57 -0400

    Added a bunch of things. Still does nothing

--- Dominion.py
+++ Dominion.py
@@ -3,34 +3,53 @@ from __future__ import print_function
 import random
 
 class game(object):
-    def __init__(self,numPlayers=2):
-        #create the initial starting piles of cards. 
+    def __init__(self,numPlayers=2,mode='original'):
         self._players = []
+        self._numPlayers = numPlayers
         for i in range(numPlayers):
-            self._players.append(player())
+            self._players.append(player(self))
+        self._playerNum = random.randint(0,numPlayers-1)
+        self._currentPlayer = self._players[self._playerNum]
+        self._trash = trash(self)
+        self._stores = []
+        self._stores.append(store(10,estate))
 
     def getPlayers(self):
         return self._players
-    
+
     def getCurrentPlayer(self):
+        return self._players[self._playerNum]
+
+    def isGameOver(self):
         pass
+
+    #iterate over each player stopping when the game is over
+    def next():
+        if self.isGameOver():
+            raise StopIteration
+        self._playerNum = (self._playerNum + 1) % self._numPlayers
+        return self._players[self._playerNum]
+
+    def __iter__(self):
+        return self
 
 class player(object):
     def __init__(self,game):
-        self._deck = deck()
+        self._deck = deck(self)
         self._game = game
+        self._money = 0
+        self._remainingActions = 0
+        self._remainingBuys = 0
         for i in range(7):
-            self._hand.append(gold1())
+            self._deck.addNew(gold1)
         for i in range(3):
-            self._hand.append(victory1())
+            self._deck.addNew(victory1)
 
     def playCard(self,card):
-        pass
+        if not self._isCardPlayable(card):
+            raise Exception('card not currently playable')
 
-    def drawCard(self,card);
-        pass
-
-    def endTurn(self):
+    def drawCard(self);
         pass
 
     def getGame(self):
@@ -39,20 +58,54 @@ class player(object):
     def getDeck(self):
         return self._deck
 
+    def addActions(self,numActions):
+        self._remainingActions += numActions
+
+    def addMoney(self,money):
+        self._money += money
+
+    def addBuys(self,buys):
+        self._remainingBuys += buys
+
+    def _newTurn(self):
+        self._money = 0
+        self._remainingActions = 1
+        self._remainingBuys = 1
+
+    def _isCardPlayable(self,card):
+        pass
+
+class store(pile):
+    """Stores are special piles that you can only take from and not ever add to.
+    They represent the cards that players can purchase. Once purchased, a card 
+    is (usually) moved from the store to the player's discard pile."""
+    def __init__(self,count,newCard):
+        pile.__init__(self)
+        for i in range(count):
+            self._cards.append(newCard(self))
+
+    def addNew(self,newCard=card):
+        raise Exception('cards cannot be added to a store')
+
 #Any pile of cards
 class pile(object):
     def __init__(self):
         self._cards = []
 
-    def add(self,newCard):
+    def addNew(self,newCard):
+        """Add a new card. The card must not have existed before adding it to 
+        the pile. If the card already exists somewhere, use card.move() instead"""
         #todo: make sure the newCard is actually a card
-        self._cards.append(newCard)
+        self._cards.append(newCard(self))
 
     def __len__(self):
         len(self._cards)
 
     def __getitem__(self,i):
         return self._cards[i]
+
+    def __delitem__(self,i):
+        pass
 
     def __contains__(self,item):
         return item in self._cards
@@ -65,15 +118,26 @@ class deck(pile):
         self._library = pile()
 
     def draw(self):
-        pass
+        if len(self._library) == 0:
+            self._refreshLibrary(self)
+        #you can't draw if your out of cards
+        if len(self._library) != 0:
+            self._library[0].move(self._hand)
 
     #add implies a card goes to your discard
     #usually this is when the card says you "gain" a card
-    def add(self,newCard):
-        self._discard.add(newCard)
+    def addNew(self,newCard):
+        self._discard.addNew(newCard)
 
     def getPlayer(self):
         return self._player
+
+    def _refreshLibrary(self):
+        if len(self._library) == 0:
+            for card in self._discard:
+                card.move(self._library)
+            self._discard = pile()
+            random.shuffle(self._library)
 
     @property
     def _cards(self):
@@ -88,16 +152,31 @@ class card(object):
         self._victoryPoints = 0
         self._cost = 0
         self._pile = pile
+        #things the card can do when played
+        self._effects = {'money':0,'cards':0,'actions':0,'buys':0}
 
-    #dummy method. override in child classes
-    def activate(self):
-        pass
+    #play the card. only does basic card operations
+    def play(self):
+        #todo: get the player
+        for i in range(self._effects['cards']):
+            player.drawCard()
+        player.addActions(self._effects['actions'])
+        player.addMoney(self._effects['money'])
+        player.addBuys(self._effects['buys'])
+        self._specialActions()
 
-    def move(self,newPile):
-        pass
+    def move(self,destPile):
+        destPile._cards.append(self)
+        self._pile._cards.remove(self)
 
     def getPile(self):
         return self._pile
+
+    def _specialActions(self):
+    """Dummy method that should be overridden in child classes who do something 
+    other than cause the player to draw cards, gain money, or gain extra 
+    actions."""
+        pass
 
     @property
     def victoryPoints(self):
@@ -108,25 +187,30 @@ class card(object):
         return self._cost
 
 #Gold cards
+#todo: figure out how to make gold cards not use up actions
 #Cost: 0
 class gold1(card):
     def __init__(self):
         card.__init__(self)
         self._cost = 0
+        self._effects['gold'] = 1
 
 #Cost: 3
 class gold2(card):
     def __init__(self):
         card.__init__(self)
         self._cost = 0
+        self._effects['gold'] = 2
 
 #Cost 6
 class gold3(card):
     def __init__(self):
         card.__init__(self)
         self._cost = 0
+        self._effects['gold'] = 3
 
 #Land (or whatever their real name is) cards
+#todo: figure out how ot make victory cards not playable
 #Cost: 2
 class victory1(card):
     def __init__(self):
@@ -188,6 +272,8 @@ class village(card):
     def __init__(self):
         card.__init__(self)
         self._cost = 3
+        self._effects['cards'] = 1
+        self._effects['actions'] = 2
 
 #Cost: 3
 #+1 Buy
@@ -196,6 +282,8 @@ class woodcutter(card):
     def __init__(self):
         card.__init__(self)
         self._cost = 3
+        self._effects['buys'] = 1
+        self._effects['gold'] = 2
 
 #Cost: 3
 #Gain a card costing up to $4
