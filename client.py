a8a8bf3daf1076b047c260ed8f866a8c5cff50fd
mfilmer <mattfilmer@gmail.com>
2012-07-25 11:05:57 -0400

    Added stuff that should make it look pretty much the same on windows and linux

--- client.py
+++ client.py
@@ -9,6 +9,7 @@ import string
 class StatusBar(object):
     def __init__(self):
         self._window = curses.newwin(1,1,23,0)
+        self._window.bkgd(' ',curses.color_pair(1))
 
 class Column(object):
     def __init__(self,(rows,cols),x=0,y=0,title='Untitled'):
@@ -20,13 +21,15 @@ class Column(object):
         self._y = y
         self._title = title
         self._titleWindow = curses.newwin(2,cols,y,x)
+        self._titleWindow.bkgd(' ',curses.color_pair(1))
         self._pad = curses.newpad(rows,cols)
+        self._pad.bkgd(' ',curses.color_pair(1))
         self._pad.scrollok(True)
         self._pad.idlok(True)
 
     def redraw(self):
         title = self._title.center(self._cols)
-        self._titleWindow.addstr(0,0,title,curses.color_pair(0) | curses.A_BOLD)
+        self._titleWindow.addstr(0,0,title,curses.color_pair(1) | curses.A_BOLD)
         self._titleWindow.hline(1,0,curses.ACS_HLINE,self._rows)
         self._titleWindow.refresh()
         self._pad.refresh(self._dataRow,0,self._y + 2,self._x,21,self._cols + \
@@ -36,7 +39,7 @@ class Column(object):
         self._rowData = data
         self._pad.erase()
         for i,text in enumerate(data):
-            self._pad.addstr(i,0,text,curses.color_pair(0))
+            self._pad.addstr(i,0,text,curses.color_pair(1))
         self._pad.clrtobot()
 
     def scrollUp(self,lines=1):
@@ -47,11 +50,20 @@ class Column(object):
 
 class Display(object):
     def __init__(self,stdscr):
+        curses.curs_set(0)
+        if curses.COLORS == 8:
+            curses.use_default_colors()
+            curses.init_pair(1,-1,-1)
+        elif curses.COLORS == 16:
+            curses.init_pair(1,0,15)
+        stdscr.bkgd(' ',curses.color_pair(1))
         self._stdscr = stdscr
         self._titleWin = curses.newwin(1,80,0,0)
-        self._titleWin.addstr(0,0,'Dominion',curses.color_pair(0) | \
+        self._titleWin.bkgd(' ',curses.color_pair(1))
+        self._titleWin.addstr(0,0,'Dominion',curses.color_pair(1) | \
                 curses.A_BOLD)
         self._borderWin = curses.newwin(24,80,0,0)
+        self._borderWin.bkgd(' ',curses.color_pair(1))
         self._borderWin.hline(1,0,curses.ACS_HLINE,80)
         self._borderWin.vline(2,26,curses.ACS_VLINE,20)
         self._borderWin.vline(2,53,curses.ACS_VLINE,20)
@@ -69,25 +81,22 @@ class Display(object):
 
 def main(stdscr):
     #curses.use_default_colors()
+    #curses.init_pair(1,0,15)
     #stdscr.bkgd(' ',curses.color_pair(1))
+    #stdscr.refresh()
+    #sleep(2)
     #curses.init_color(15,500,0,0)
     #curses.init_pair(1,0,15)
     #curses.init_pair(2,9,15)
     #curses.init_pair(3,12,15)
     #curses.init_pair(4,14,15)
 
-    curses.use_default_colors()
-    curses.curs_set(0)
-    stdscr.bkgd(' ',curses.color_pair(1))
-    curses.init_pair(1,-1,-1)
-
-
     display = Display(stdscr)
     data = []
     for i in range(100):
         data.append('row ' + str(i))
     display._leftColumn.setRowData(data)
-    while True:
+    for i in range(1):
         for i in range(10):
             display.redraw()
             sleep(.5)
