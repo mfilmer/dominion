0be6d3c34ba1e614d7c40fefc74cbc9d7e640dc1
mfilmer <mattfilmer@gmail.com>
2012-07-26 13:49:34 -0400

    Started working on the server. Also, moved most of the UI code into clientUI.py to make room for network related code in client.py Everything still works as it should (i think)

---
+++ clientUI.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+
+import curses
+import curses.wrapper
+#import curses.textpad
+from time import sleep      #here for test purposes only. will be removed
+import string
+
+class StatusBar(object):
+    def __init__(self):
+        self._window = curses.newwin(1,80,23,0)
+        self._window.keypad(True)
+        self._window.bkgd(' ',curses.color_pair(1))
+        self._status = ''
+        self.setStatus('test status')
+
+    def setStatus(self,newStatus):
+        self._status = newStatus
+        self._window.erase()
+        self._window.addstr(0,0,newStatus,curses.color_pair(1))
+        self._window.refresh()
+
+    def getCh(self):
+        return self._window.getch()
+
+    def refresh(self):
+        self._window.refresh()
+
+class Column(object):
+    def __init__(self,cols,x=0,y=0,title='Untitled',height=21):
+        self._rowData = ['']
+        self._numRows = 0
+        self._markedRows = set()
+        self._scrollOffset = 0
+        self._selectedRow = 0
+        self.__isActive = False
+        self._cols = cols
+        self._x = x
+        self._y = y
+        self._height = 18
+        self._titleWindow = curses.newwin(2,cols,y,x)
+        self._titleWindow.bkgd(' ',curses.color_pair(1))
+        self.setTitle(title[:])
+        self._newPad(5)
+
+    @property
+    def _isActive(self):
+        return self.__isActive
+    @_isActive.setter
+    def _isActive(self,value):
+        if value:
+            self.__isActive = True
+            self._select(self._selectedRow)
+        else:
+            self.__isActive = False
+            self._deselect(self._selectedRow)
+
+    def redraw(self):
+        title = self._title.center(self._cols)
+        self._titleWindow.addstr(0,0,title,curses.color_pair(1) | curses.A_BOLD)
+        self._titleWindow.hline(1,0,curses.ACS_HLINE,self._cols)
+        self._titleWindow.refresh()
+        self._pad.refresh(self._scrollOffset,0,self._y + 2,self._x,\
+                self._height + 3,self._cols + self._x)
+
+    def setTitle(self,newTitle):
+        self._title = newTitle[:]
+        self._titleWindow.erase()
+        title = self._title.center(self._cols)
+        self._titleWindow.addstr(0,0,title,curses.color_pair(1) | curses.A_BOLD)
+        self._titleWindow.hline(1,0,curses.ACS_HLINE,self._cols)
+        self._titleWindow.refresh()
+
+    def _newPad(self,rows):
+        self._pad = curses.newpad(rows,self._cols)
+        self._pad.bkgd(' ',curses.color_pair(1))
+        self._numLines = rows
+
+    def setRowData(self,data):
+        self._rowData = data[:] #make a copy of the data
+        del(self._pad)
+        self._numRows = len(self._rowData)
+        self._newPad(self._numRows)
+        for i,text in enumerate(self._rowData):
+            if i == self._selectedRow and self._isActive:
+                if self._isActive:
+                    self._pad.addstr(i,0,text,curses.color_pair(1) | \
+                            curses.A_REVERSE)
+            else:
+                self._pad.addstr(i,0,text,curses.color_pair(1))
+        self._markedRows = set()
+
+    def scroll(self,lines=1):
+        if self._scrollOffset + lines < 0:
+            self._scrollOffset = 0
+        elif self._scrollOffset + lines + self._height > self._numRows:
+            if self._numRows < self._height:
+                self._scrollOffset = 0
+            else:
+                self._scrollOffset = self._numRows - self._height
+        else:
+            self._scrollOffset += lines
+        self.redraw()
+
+    def _deselect(self,row):
+        if row in self._markedRows:
+            self._pad.addstr(row,0,self._rowData[row],curses.color_pair(2))
+        else:
+            self._pad.addstr(row,0,self._rowData[row],curses.color_pair(1))
+
+    def _select(self,row):
+        self._pad.addstr(row,0,self._rowData[row], \
+                curses.color_pair(1) | curses.A_REVERSE)
+
+    def getSelectionOffset(self):
+        if self._selectedRow < self._scrollOffset:
+            return self._selectedRow - self._scrollOffset
+        elif self._selectedRow >= self._scrollOffset + self._height:
+            return self._selectedRow - self._height - self._scrollOffset + 1
+        else:
+            return 0
+
+    def toggleMark(self,row):
+        if row in self._markedRows:
+            self._deselect(row)
+            self._markedRows.remove(row)
+        else:
+            self._pad.addstr(row,0,self._rowData[row],curses.color_pair(2))
+            self._markedRows.add(row)
+        self.redraw()
+
+    def moveSelection(self,lines=1):
+        self._deselect(self._selectedRow)
+        if self._selectedRow + lines < 0:
+            self._selectedRow = 0
+        elif self._selectedRow + lines > self._numRows - 1:
+            self._selectedRow = self._numRows - 1
+        else:
+            self._selectedRow += lines
+        self._select(self._selectedRow)
+        #scroll if needed
+        self.scroll(self.getSelectionOffset())
+        self.redraw()
+
+class Display(object):
+    def __init__(self,stdscr):
+        curses.curs_set(0)
+        if curses.COLORS == 8:          #gnome-terminal
+            curses.use_default_colors()
+            curses.init_pair(1,-1,-1)
+            curses.init_pair(2,4,-1)    #marked
+        elif curses.COLORS == 16:       #windows command prompt
+            curses.init_pair(1,0,15)
+            curses.init_pair(2,9,15)    #marked (blue on white)
+        stdscr.bkgd(' ',curses.color_pair(1))
+        self._stdscr = stdscr
+        self._titleWin = curses.newwin(1,80,0,0)
+        self._titleWin.bkgd(' ',curses.color_pair(1))
+        self._titleWin.addstr(0,0,'Dominion',curses.color_pair(1) | \
+                curses.A_BOLD)
+        self._borderWin = curses.newwin(24,80,0,0)
+        self._borderWin.bkgd(' ',curses.color_pair(1))
+        self._borderWin.hline(1,0,curses.ACS_HLINE,80)
+        self._borderWin.vline(2,26,curses.ACS_VLINE,20)
+        self._borderWin.vline(2,53,curses.ACS_VLINE,20)
+        self._borderWin.hline(22,0,curses.ACS_HLINE,80)
+        self._leftColumn = Column(26,x=0,y=2,title='Test')
+        self._centerColumn = Column(26,x=27,y=2,title='Test')
+        self._rightColumn = Column(26,x=54,y=2,title='Test')
+        self._statusBar = StatusBar()
+        self._currentCol = self._leftColumn
+        self._currentCol._isActive = True
+        self._redraw()
+
+    def _redraw(self):
+        self._borderWin.refresh()
+        self._titleWin.refresh()
+        self._leftColumn.redraw()
+        self._centerColumn.redraw()
+        self._rightColumn.redraw()
+
+    def scroll(self,lines=1):
+        self._currentCol.scroll(lines)
+
+    def moveSelection(self,lines=1):
+        self._currentCol.moveSelection(lines)
+
+    def changeColSelect(self,num):
+        if num == 0:
+            raise ValueError
+        if num < 0:
+            if self._currentCol == self._centerColumn:
+                self._centerColumn._isActive = False
+                self._currentCol = self._leftColumn
+                self._centerColumn.redraw()
+            elif self._currentCol == self._rightColumn:
+                self._rightColumn._isActive = False
+                self._currentCol = self._centerColumn
+                self._rightColumn.redraw()
+        elif num > 0:
+            if self._currentCol == self._centerColumn:
+                self._centerColumn._isActive = False
+                self._currentCol = self._rightColumn
+                self._centerColumn.redraw()
+            elif self._currentCol == self._leftColumn:
+                self._leftColumn._isActive = False
+                self._currentCol = self._centerColumn
+                self._leftColumn.redraw()
+        self._currentCol._isActive = True
+        self._currentCol.redraw()
+
+    def toggleMark(self):
+        self._currentCol.toggleMark(self._currentCol._selectedRow)
+
+    def getCh(self):
+        return self._statusBar.getCh()
--- server.py
+++ server.py
@@ -4,49 +4,64 @@ from twisted.internet.protocol import Factory
 from twisted.protocols.basic import LineReceiver
 from twisted.internet import reactor
 
+import argparse
+
 import Dominion
 
-class Chat(LineReceiver):
-    def __init__(self,users):
+class Player(LineReceiver):
+    def __init__(self,users,maxPlayers=2):
+        self.maxPlayers = maxPlayers
         self.users = users
         self.name = None
-        self.state = 'GETNAME'
+        self.state = 'New'
 
     def connectionMade(self):
-        self.sendLine('What is your name?')
+        playerCount = len(self.users)
+        self.sendLine(playerCount)
+        for user in self.users:
+            self.sendLine(user)
 
     def connectionLost(self,reason):
         if self.users.has_key(self.name):
             del self.users[self.name]
 
-    def lineReceived(self,line):
-        if self.state == 'GETNAME':
-            self.handle_GETNAME(line)
-        else:
-            self.handle_CHAT(line)
-
-    def handle_GETNAME(self,name):
+    def registerNewPlayer(self,name):
         if self.users.has_key(name):
-            self.sendLine('Name taken, please choose another.')
-            return
-        self.sendLine('Welcome, %s!' % (name,))
-        self.name = name
-        self.users[name] = self
-        self.state = 'CHAT'
+            self.sendLine('taken')
+        else:
+            self.name = line
+            self.state = 'InLobby'
+            for name,protocol in self.users.iteritems():
+                self.sendLine('newplayer ' + line)
+            if len(self.users) == self.maxPlayers:
+                for name,protocol in self.users.iteritems():
+                    self.sendLine('starting')
+                    protocol.state = 'Starting'
 
-    def handle_CHAT(self,message):
-        message = '<%s> %s' % (self.name,message)
-        for name, protocol in self.users.iteritems():
-            if protocol != self:
-                protocol.sendLine(message)
+    def lineReceived(self,line):
+        if self.state == 'New': #they sent their name. tell everyone about it
+            self.registerNewPlayer(line)
+        elif self.state == 'InLobby':
+            pass    #perhaps this is a chat message. implement later
+        else:       #sould not have received this line
+            print('received bad line from ' + self.name + ': ')
+            print(line)
 
-class ChatFactory(Factory):
+class GameFactory(Factory):
     def __init__(self):
         self.users = {}
 
     def buildProtocol(self,addr):
-        return Chat(self.users)
+        return Player(self.users)
 
-reactor.listenTCP(6814,ChatFactory())
-reactor.run()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p','--port',type=int,default=6814)
+    parser.add_argument('-P','--players',type=int,default=2)
+    args = parser.parse_args()
+    
+    reactor.listenTCP(args.port,ChatFactory())
+    reactor.run()
 
+if __name__ == '__main__':
+    main()
